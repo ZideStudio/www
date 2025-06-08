6b4ca9b6deddced37ebb4f4904bb39145d27c52e
Jules Sorensen <julessorens@gmail.com>
2025-06-08 18:38:15 +0200

    fix: seo meta description

--- src/components/Head/index.tsx
+++ src/components/Head/index.tsx
@@ -14,9 +14,9 @@ export const Head = ({ screen, pathLocation, title, description }: HeadProps) =>
   return (
     <Helmet>
       <title>{isWelcomeScreen ? 'Zide' : (title ?? 'Projects')}</title>
-      <meta name="description" content={isWelcomeScreen ? 'Digital simplicity, greater efficiency.' : 'Explore our innovative projects designed for a greater efficiency and digital simplicity.'} />
+      <meta name="description" content={description ?? (isWelcomeScreen ? "Digital simplicity, greater efficiency. We're producing innovative and useful tools for our users!" : 'Explore our innovative projects designed for digital simplicity and a greater efficiency.')} />
       <meta property="og:title" content={isWelcomeScreen ? 'Zide' : (title ?? 'Projects')} />
-      <meta property="og:description" content={description ?? (isWelcomeScreen ? "Digital simplicity, greater efficiency. We're producing innovative and useful tools for our users." : "Discover the tools and solutions we're creating to enhance user experiences and drive innovation.")} />
+      <meta property="og:description" content={description ?? (isWelcomeScreen ? "Digital simplicity, greater efficiency. We're producing innovative and useful tools for our users!" : "Discover the tools and solutions we're creating to enhance user experiences and drive innovation.")} />
       <meta property="og:image" content="/assets/logo/zide_complete.png" />
       <meta name="google-site-verification" content="oSycr6s-tbcxcGBjTexUhsgH0NinsvxBXBhOokbBPRk" />
       <link rel="canonical" href={`https://zide.fr${pathLocation}`} />
