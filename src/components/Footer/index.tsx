b7d2aadf6d9d563a45e9e9863a1ca85a969a136f
Jules Sorensen <julessorens@gmail.com>
2025-06-08 16:53:45 +0200

    fix: footer tags

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -16,50 +16,54 @@ export const Footer = () => {
         <div className="w-full px-12 md:px-0 md:w-[13rem]">
           <p className="text-start text-xl font-semibold">Collective</p>
           <hr className="border-gray-600 my-4 w-1/3" />
-          <p className="text-start mt-2">Zide is a software development collective creating innovative tools for everyone.</p>
-          <p className="text-start mt-1 text-xs text-gray-400">Driven by passion and curiosity, We craft digital products that are intuitive, efficient and highly customizable, built to simplify the user experience while unlocking powerful workflows.</p>
+          <ul>
+            <li className="text-start mt-2">Zide is a software development collective creating innovative tools for everyone.</li>
+            <li className="text-start mt-1 text-xs text-gray-400">Driven by passion and curiosity, We craft digital products that are intuitive, efficient and highly customizable, built to simplify the user experience while unlocking powerful workflows.</li>
+          </ul>
         </div>
         {/* Projects */}
         <div className="w-full px-12 md:px-0 md:w-[13rem]">
           <p className="text-start text-xl font-semibold">Projects</p>
           <hr className="border-gray-600 my-4 w-1/3" />
-          {PROJECTS.map((project) => (
-            <div key={project.id} className="flex mt-2">
-              <a
-                className={`text-start font-semibold ${project.status === ProjectStatus.RELEASED ? 'text-white' : 'text-gray-300'} hover:text-blue-400 transition-colors duration-300`}
-                href={`/project/${project.slug}`}
-                onClick={(e) => {
-                  e.preventDefault();
-                  navigate(`/project/${project.slug}`);
-                }}
-              >
-                {project.title}
-              </a>
-            </div>
-          ))}
+          <ul>
+            {PROJECTS.map((project) => (
+              <li key={project.id} className="flex mt-2">
+                <a
+                  className={`text-start font-semibold ${project.status === ProjectStatus.RELEASED ? 'text-white' : 'text-gray-300'} hover:text-blue-400 transition-colors duration-300`}
+                  href={`/project/${project.slug}`}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    navigate(`/project/${project.slug}`);
+                  }}
+                >
+                  {project.title}
+                </a>
+              </li>
+            ))}
+          </ul>
         </div>
         {/* Contact */}
         <div className="w-full px-12 md:px-0 md:w-[13rem]">
           <p className="text-start text-xl font-semibold">Contact</p>
           <hr className="border-gray-600 my-4 w-1/3" />
-          <p className="text-start mt-2">
-            <div className="flex flex-row items-center space-x-3 mt-2">
+          <ul className="text-start mt-2">
+            <li className="flex flex-row items-center space-x-3 mt-2">
               <i className="pi pi-home" style={{ fontSize: '1rem' }} />
               <p>France</p>
-            </div>
-            <div className="flex flex-row items-center space-x-3 mt-2">
+            </li>
+            <li className="flex flex-row items-center space-x-3 mt-2">
               <i className="pi pi-twitter" style={{ fontSize: '1rem' }} />
-              <a href="https://x.com/ZideStudio" className="text-blue-400">
+              <a href="https://x.com/ZideStudio" className="text-blue-400" target="_blank" rel="noopener noreferrer">
                 @ZideStudio
               </a>
-            </div>
-            <div className="flex flex-row items-center space-x-3 mt-2">
+            </li>
+            <li className="flex flex-row items-center space-x-3 mt-2">
               <i className="pi pi-envelope" style={{ fontSize: '1rem' }} />
               <a href="mailto:contact@zide.fr" className="text-blue-400">
                 contact@zide.fr
               </a>
-            </div>
-          </p>
+            </li>
+          </ul>
         </div>
       </div>
       <div className="bg-[#1F1F1F] py-2 flex flex-row space-x-2 justify-center">
