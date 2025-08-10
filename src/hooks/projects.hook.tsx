b880cd2fde1568f381ad69f37c3b32385031f453
Jules Juul <julessorens@gmail.com>
2025-08-10 15:01:06 +0200

    fix: project hook ts errors

--- src/hooks/projects.hook.tsx
+++ src/hooks/projects.hook.tsx
@@ -20,27 +20,35 @@ export const useProjects = ({ filters }: UseProjectsProps) => {
 
   // filtering
   if (filters.name) {
-    projects = projects.filter((project) => project.title.toLowerCase().includes(filters.name?.toLowerCase()));
+    projects = projects.filter((project) => project.title.toLowerCase().includes(filters.name!.toLowerCase()));
   }
   if (filters.target) {
-    projects = projects.filter((project) => project.target.toLowerCase().includes(filters.target?.toLowerCase()));
+    projects = projects.filter((project) => project.target.toLowerCase().includes(filters.target!.toLowerCase()));
   }
   if (filters.status) {
-    projects = projects.filter((project) => project.status.toLowerCase().includes(filters.status?.toLowerCase()));
+    projects = projects.filter((project) => project.status.toLowerCase().includes(filters.status!.toLowerCase()));
   }
   if (filters.type) {
-    projects = projects.filter((project) => project.type.toLowerCase().includes(filters.type?.toLowerCase()));
+    projects = projects.filter((project) => project.type.toLowerCase().includes(filters.type!.toLowerCase()));
   }
 
   // sorting
   if (filters.sort === 'oldest') {
-    projects = projects.sort((a, b) => (a.releaseDate.date ?? a.releaseDate.planned) - (b.releaseDate.date ?? b.releaseDate.planned));
+    projects = projects.sort((a, b) => {
+      const aDate = a.releaseDate?.date ?? a.releaseDate?.planned ?? 0;
+      const bDate = b.releaseDate?.date ?? b.releaseDate?.planned ?? 0;
+      return Number(aDate) - Number(bDate);
+    });
   } else if (filters.sort === 'alphabetical') {
     projects = projects.sort((a, b) => a.title.localeCompare(b.title));
   } else if (filters.sort === 'reverseAlphabetical') {
     projects = projects.sort((a, b) => b.title.localeCompare(a.title));
   } else {
-    projects = projects.sort((a, b) => (b.releaseDate.date ?? b.releaseDate.planned) - (a.releaseDate.date ?? a.releaseDate.planned));
+    projects = projects.sort((a, b) => {
+      const aDate = a.releaseDate?.date ?? a.releaseDate?.planned ?? 0;
+      const bDate = b.releaseDate?.date ?? b.releaseDate?.planned ?? 0;
+      return Number(bDate) - Number(aDate);
+    });
   }
 
   if (!filters.status) {
