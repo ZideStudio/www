c4aa72d7e15b632db215ddafcabe6a7161bb6940
Jules Juul <julessorens@gmail.com>
2025-08-18 20:58:13 +0200

    feat(#14): no project found message
    Issue url: https://github.com/ZideStudio/www/issues/14

--- src/app/[locale]/projects/client.tsx
+++ src/app/[locale]/projects/client.tsx
@@ -3,6 +3,7 @@
 import { useProjects } from '@/hooks/projects.hook';
 import { ProjectFilters } from '@components/ProjectFilters';
 import { ProjectGrid } from '@components/ProjectGrid';
+import { useTranslations } from 'next-intl';
 import { useState } from 'react';
 
 export const Projects = () => {
@@ -11,6 +12,8 @@ export const Projects = () => {
   const [status, setStatus] = useState('');
   const [type, setType] = useState('');
   const [sort, setSort] = useState('newest');
+
+  const t = useTranslations('projects');
 
   const { projects } = useProjects({
     filters: {
@@ -26,9 +29,16 @@ export const Projects = () => {
     <div className="mx-5 space-y-5 md:mx-24">
       <ProjectFilters setNameAction={setName} setTargetAction={setTarget} setStatusAction={setStatus} setTypeAction={setType} setSortAction={setSort} />
 
-      <section>
-        <ProjectGrid projects={projects} />
-      </section>
+      {projects.length > 0 ? (
+        <section>
+          <ProjectGrid projects={projects} />
+        </section>
+      ) : (
+        <div className="flex flex-col items-center justify-center py-10">
+          <p className="text-text text-xl font-bold">{t('errors.not_found')}</p>
+          <p className="text-text/50">{t('errors.not_found_details')}</p>
+        </div>
+      )}
     </div>
   );
 };
